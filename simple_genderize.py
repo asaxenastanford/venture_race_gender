0ae3795f069138ae9a00edf6a1226c222ea4f76c
Aakanksha Saxena <asaxena1@stanford.edu>
2019-06-10 16:05:21 -0700

    script used to parse list of investor names, call the genderize api to calculate initial gender predictions, and write results to excel

---
+++ simple_genderize.py
@@ -0,0 +1,69 @@
+from genderize import Genderize
+import xlrd
+import openpyxl
+import requests
+import os, shutil
+
+# to search 
+excelFile = '/Users/aakankshasaxena/Documents/Sophomore/Strebulaev/genderize/Venture_Capital_List_Mod.xlsx'
+wb = xlrd.open_workbook(excelFile) 
+sheet = wb.sheet_by_index(0) #read
+xfile = openpyxl.load_workbook('/Users/aakankshasaxena/Documents/Sophomore/Strebulaev/genderize/Venture_Capital_List_Mod.xlsx')
+sheet2 = xfile['Sheet1'] #xfile.get_sheet_by_name('Sheet1') #write
+
+xfile1 = openpyxl.load_workbook('/Users/aakankshasaxena/Documents/Sophomore/Strebulaev/genderize/names_to_run.xlsx')
+javasheetwrite = xfile1['Sheet1'] #xfile.get_sheet_by_name('Sheet1')
+javaFile = '/Users/aakankshasaxena/Documents/Sophomore/Strebulaev/genderize/names_to_run.xlsx'
+wbJ = xlrd.open_workbook(javaFile)
+javasheetread = wbJ.sheet_by_index(0)
+
+currentLine = javasheetread.cell_value(0, 1)
+currentLineNumber = round(currentLine)
+total = javasheetread.cell_value(0, 3)
+totalNumber = round(total)
+api_string = "df689b8f770275905ab667f3ed4b3191"
+
+#1677
+#3763
+for r in range(29927,32046):
+	firstName = sheet.cell_value(r,1)
+	lastName = sheet.cell_value(r,2)
+	result = (Genderize(api_key = api_string).get([firstName]))
+	#print(type(result))
+	dictionary = result[0]
+	name = dictionary['name']
+	gender = dictionary['gender']
+	genderIndex = "D" + str(r + 1)
+	sheet2[genderIndex] = gender
+
+	if(gender == None):
+		firstNameIndex = "A" + str(currentLineNumber)
+		lastNameIndex = "B" + str(currentLineNumber)
+		actualIndex = "C" + str(currentLineNumber)
+
+		javasheetwrite[firstNameIndex] = firstName
+		javasheetwrite[lastNameIndex] = lastName
+		javasheetwrite[actualIndex] = (r + 1)
+		currentLineNumber += 1
+		totalNumber += 1
+	else:
+		probability = dictionary['probability']
+		probIndex = "E" + str(r + 1)
+		sheet2[probIndex] = probability
+
+		count = dictionary['count']
+		countIndex = "F" + str(r + 1)
+		sheet2[countIndex] = count
+
+	if(r%25==0):
+		print(dictionary)
+		javasheetwrite["B1"] = currentLineNumber
+		javasheetwrite["D1"] = totalNumber
+		xfile.save('/Users/aakankshasaxena/Documents/Sophomore/Strebulaev/genderize/Venture_Capital_List_Mod.xlsx')
+		xfile1.save('/Users/aakankshasaxena/Documents/Sophomore/Strebulaev/genderize/names_to_run.xlsx')
+
+javasheetwrite["B1"] = currentLineNumber
+javasheetwrite["D1"] = totalNumber
+
+xfile.save('/Users/aakankshasaxena/Documents/Sophomore/Strebulaev/genderize/Venture_Capital_List_Mod.xlsx')
+xfile1.save('/Users/aakankshasaxena/Documents/Sophomore/Strebulaev/genderize/names_to_run.xlsx')
